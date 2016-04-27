8ebda34b0171abb8fece40651656b4140060f175
minhyeong-kim <minhyeong.kim@nhnent.com>
2016-04-27 15:40:35 +0900

    fix: textSelection, imageDrag bug

--- src/domutil.js
+++ src/domutil.js
@@ -359,7 +359,7 @@ export function disableTextSelection() {
     var style;
 
     if (SUPPORT_SELECTSTART) {
-        domevent.on(window, 'selectstart', preventDefault);
+        domevent.on(document, 'selectstart', preventDefault);
     } else {
         style = document.documentElement.style;
         prevSelectStyle = style[userSelectProperty];
@@ -372,7 +372,7 @@ export function disableTextSelection() {
  */
 export function enableTextSelection() {
     if (SUPPORT_SELECTSTART) {
-        domevent.off(window, 'selectstart', preventDefault);
+        domevent.off(document, 'selectstart', preventDefault);
     } else {
         document.documentElement.style[userSelectProperty] = prevSelectStyle;
     }
@@ -382,14 +382,14 @@ export function enableTextSelection() {
  * Disable browser's image drag behaviors.
  */
 export function disableImageDrag() {
-    domevent.on(window, 'dragstart', preventDefault);
+    domevent.on(document, 'dragstart', preventDefault);
 }
 
 /**
  * Enable browser's image drag behaviors.
  */
 export function enableImageDrag() {
-    domevent.off(window, 'dragstart', preventDefault);
+    domevent.off(document, 'dragstart', preventDefault);
 }
 
 /**
