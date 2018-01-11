fccfdfbee7ab4f3813746bd38e6f4f8550f2a62f
seonim-ryu <seonim.ryu@nhnent.com>
2018-01-11 18:34:54 +0900

    style: add newline before return statement

--- src/js/domutil.js
+++ src/js/domutil.js
@@ -86,6 +86,7 @@ function setClassName(element, cssClass) {
 
     if (snippet.isUndefined(element.className.baseVal)) {
         element.className = cssClass;
+
         return;
     }
 
@@ -108,6 +109,7 @@ export function addClass(element) {
         snippet.forEach(cssClass, name => {
             classList.add(name);
         });
+
         return;
     }
 
@@ -143,6 +145,7 @@ export function toggleClass(element) {
         snippet.forEach(cssClass, name => {
             element.classList.toggle(name);
         });
+
         return;
     }
 
